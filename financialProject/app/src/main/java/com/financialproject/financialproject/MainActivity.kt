80e09b3ea00b73944443c7a5b7d8474fc6801fb6
JuanRamirezMendoza <ramirezsebastian72@gmail.com>
2022-01-07 11:09:23 -0500

    status barr transparent and add incomingView

--- financialProject/app/src/main/java/com/financialproject/financialproject/MainActivity.kt
+++ financialProject/app/src/main/java/com/financialproject/financialproject/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.financialproject.financialproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.financialproject.financialproject.databinding.ActivityMainBinding
+import android.view.WindowManager
+
+import android.os.Build
+import android.view.Window
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // In Activity's onCreate() for instance
+        val w: Window = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        binding.helloWorld.setOnClickListener{
+            val intent = Intent(this,InconmingView::class.java)
+            startActivity(intent)
+        }
     }
 }
